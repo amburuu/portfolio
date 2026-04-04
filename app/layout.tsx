230bb44fc59b09c602ed38aa04f33f0d38780a2c
Ambre <ambrenguy@gmail.com>
2026-04-04 19:17:37 +0200

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-  
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Chaos" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="min-h-full flex flex-col"><Auth0Provider>{children}</Auth0Provider></body>
     </html>
