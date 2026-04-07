2599605d612ce00ea4addfe68842eb3b7fcfc60d
Ambre <ambrenguy@gmail.com>
2026-04-07 23:15:47 +0200

    Add texture and interaction to 3D WIP

--- src/components/ChaosModel.tsx
+++ src/components/ChaosModel.tsx
@@ -1,18 +1,28 @@
-import { useGLTF, Center } from "@react-three/drei";
+import { useGLTF, MeshTransmissionMaterial } from "@react-three/drei";
 import { useRef } from "react";
-import { Group } from "three"
+import { Group } from "three";
+import { useControls } from "leva";
 
+useGLTF.preload('/chaos3D.glb');
 
-useGLTF.preload('/chaos3D.glb')
+export default function ChaosModel() {
+    const outer = useRef<Group>(null);
+    const { nodes } = useGLTF('/chaos3D.glb') as any;
 
-export default function ChaosModel(){
-    const group = useRef<Group>(null);
-    const { scene } = useGLTF('/chaos3D.glb');
+    const config = useControls({
+        backside: true,
+        transmission: { value: 1, min: 0, max: 1 },
+        roughness: { value: 0.0, min: 0, max: 1, step: 0.01 },
+        thickness: { value: 0.20, min: 0, max: 10, step: 0.01 },
+        ior: { value: 1.5, min: 1, max: 5, step: 0.01 },
+        chromaticAberration: { value: 0.06, min: 0, max: 1 },
+    });
+
     return (
-         <Center>
-            <group ref={group}>
-                <primitive object={scene} />
-            </group>
-        </Center>
+        <group ref={outer}>
+            <mesh geometry={nodes.Curve.geometry}>
+                <MeshTransmissionMaterial {...config} />
+            </mesh>
+        </group>
     );
 }
--- src/components/ChaosScene.tsx
+++ src/components/ChaosScene.tsx
@@ -1,21 +1,30 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import { Suspense } from "react";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Leva } from "leva";
 import ChaosModel from "./ChaosModel";
-
 
 export default function ChaosScene() {
     return (
-        <div className="w-full aspect-square max-h-[60vh]">
-            <Canvas camera={{ position: [0, 0, 15], fov: 20 }}>
+        <div className="w-full h-[50vh]">
+            <Leva collapsed />
+            <Canvas camera={{ position: [0, 0, 20], fov: 40 }}>
                 <ambientLight intensity={1} />
                 <directionalLight position={[10, 10, 5]} intensity={1} />
                 <Suspense fallback={null}>
-                    <ChaosModel />
+                <Environment preset="park" />
+                <ChaosModel />
                 </Suspense>
-                <OrbitControls />
+                <OrbitControls
+                    enableZoom={false}
+                    enablePan={false}
+                    minPolarAngle={Math.PI / 2}
+                    maxPolarAngle={Math.PI / 2}
+                />
             </Canvas>
         </div>
     );
 }
+
+// apartment, city, dawn, forest, lobby, night, park, studio, sunset, warehouse
